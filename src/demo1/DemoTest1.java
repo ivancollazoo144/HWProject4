0c4972a7482c878f6aac23aad1971e9b158b71f1
ivancollazoo <collazoivan1412@gmail.com>
2025-01-31 16:25:00 -0400

    Coding is fun commit

---
+++ src/demo1/DemoTest1.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class DemoTest1 {
+	public static void main(String[] args) {
+		
+		int Java = 2025;
+		int Python = 2026;
+		
+		
+		if(Java == 2025 & Python == 2026) {
+			System.out.println("Coding is fun");
+		}
+		
+	}
+
+}
